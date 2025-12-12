962384273cef03c3064cca1e7298cb2005b96ebf
Atte Räty <raty.atte@gmail.com>
2025-12-12 14:19:48 +0200

    Add kirjakirppis to portfolio

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -12,7 +12,7 @@
           Vahlaz.ᵈᵉᵛ
         </h1>
         <h2
-          class="text-4xl max-w-[65vw] shadow-2xl mt-2 rounded-2xl p-2 shadow-gray-800/45 bg-gray-800/30"
+          class="text-3xl md:text-4xl text-center max-w-[65vw] shadow-2xl mt-2 rounded-2xl p-2 shadow-gray-800/45 bg-gray-800/30"
         >
           Hi, I'm Atte! A developer and computer science student from Aalto
           university
@@ -76,17 +76,20 @@
           class="flex flex-col columns-1 justify-center align-middle items-center h-full w-full position: relative"
         >
           <div
-            class="text-6xl p-4 max-w-[90vw] font-bold overflow-hidden wrap-break-word"
+            class="text-5xl md:text-6xl p-4 max-w-[90vw] font-bold overflow-hidden wrap-break-word"
           >
-            FutuBoard
+            Futuboard
           </div>
-          <div class="text-2xl p-4 max-w-225 overflow-hidden">
+          <div class="text-xl md:text-2xl p-4 max-w-225 overflow-hidden">
             Futuboard is a free, easy-to-use and open-source web tool for
             workflow management that requires no registration. It can be used to
             manage individual or team work efficiently and remotely. It allows
             for an easier to use platform than alternative tools with the same
             features, and it includes automatic visualizations and statistics
             about the progress of work.
+          </div>
+          <div class="text-lg md:text-xl p-4 max-w-150 overflow-hidden">
+            React, Redux, Django, Typescript, Python, CSS, Scrum
           </div>
           <a
             href="https://futuboard.live/"
@@ -110,7 +113,7 @@
           class="flex flex-col columns-1 justify-center align-middle items-center h-full w-full position: relative"
         >
           <div
-            class="text-6xl p-4 max-w-[90vw] overflow-hidden wrap-break-word"
+            class="text-5xl md:text-6xl p-4 max-w-[90vw] font-bold overflow-hidden wrap-break-word"
           >
             KaraSaber
           </div>
@@ -120,7 +123,9 @@
             and lyric file to the resources folder. Buttons are created
             dynamically for added songs.
           </div>
-
+          <div class="text-lg md:text-xl p-4 max-w-150 overflow-hidden">
+            C#, Unity, VR
+          </div>
           <a
             href="https://github.com/Vahlaz/KaraSaber"
             class="position: absolute flex justify-center bottom-5 outline-2 bg-gray-600 hover:bg-gray-700 transition duration-150 outline-gray-800 rounded-xl w-30"
@@ -137,7 +142,7 @@
           class="flex flex-col columns-1 justify-center align-middle items-center h-full w-full position: relative backdrop-brightness-50 bg-contain"
         >
           <div
-            class="text-6xl p-4 max-w-[90vw] overflow-hidden wrap-break-word"
+            class="text-5xl md:text-6xl p-4 max-w-[90vw] font-bold overflow-hidden wrap-break-word"
           >
             ByTheLake
           </div>
@@ -146,6 +151,9 @@
             landscape in the summer, where they can explore the environment,
             listen to nature's sounds, and feel the calmness brought by the
             forest and water.
+          </div>
+          <div class="text-lg md:text-xl p-4 max-w-150 overflow-hidden">
+            C#, Unity, VR
           </div>
           <a
             href="https://youtu.be/91lK5Wssfn0"
@@ -163,13 +171,43 @@
       </div>
       <div
         alt="backdrop"
+        class="bg-[url(../assets/Kirjakirppis.png)] bg-cover bg-center transition-all delay-75 duration-200 w-300 h-200 max-w-[95vw] text-white rounded-xl shadow-xl outline-1 outline-gray-700 shadow-gray-800 m-8"
+      >
+        <div
+          class="flex flex-col columns-1 justify-center align-middle items-center h-full w-full position: relative"
+        >
+          <div
+            class="text-5xl md:text-6xl p-4 max-w-[90vw] font-bold overflow-hidden wrap-break-word"
+          >
+            KirjaKirppis
+          </div>
+          <div class="text-2xl p-4 max-w-225 overflow-hidden">
+            KirjaKirppis is a simple website made for selling and purchasing
+            second hand high school course books. It has specific sites for all
+            high schools in finland, as well as icons for all course books
+            officially in use by the schools. It also has users with token based
+            authentication.
+          </div>
+          <div class="text-lg md:text-xl p-4 max-w-150 overflow-hidden">
+            React, NodeJS, GraphQl, Material UI
+          </div>
+          <a
+            href="https://github.com/Vahlaz/kirjakirppis"
+            class="position: absolute flex justify-center bottom-5 outline-2 bg-gray-600 hover:bg-gray-700 transition duration-150 outline-gray-800 rounded-xl w-30"
+          >
+            code
+          </a>
+        </div>
+      </div>
+      <div
+        alt="backdrop"
         class="bg-[url(../assets/ScuffedPuolustus.png)] bg-cover bg-center transition-all delay-75 duration-200 w-300 h-200 max-w-[95vw] text-white rounded-xl shadow-xl outline-1 outline-gray-700 shadow-gray-800 m-8"
       >
         <div
           class="flex flex-col columns-1 justify-center align-middle items-center h-full w-full position: relative"
         >
           <div
-            class="text-6xl p-4 max-w-[90vw] overflow-hidden wrap-break-word"
+            class="text-5xl md:text-6xl p-4 max-w-[90vw] font-bold overflow-hidden wrap-break-word"
           >
             ScuffedPuolustus
           </div>
@@ -179,6 +217,9 @@
             player earns money by destroying enemies, and with the money earned,
             the player can place more towers. The number and strength of enemies
             increase with each round.
+          </div>
+          <div class="text-lg md:text-xl p-4 max-w-150 overflow-hidden">
+            Scala
           </div>
           <a
             href="https://github.com/Vahlaz/ScuffedPuolustus"
